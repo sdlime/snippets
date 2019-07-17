0ef51c7f19edf7d8beaa55386c310cf3b066af5e
greenwoodmap <richard.greenwood@gmail.com>
2019-07-17 10:23:51 -0600

    added variables for leaflet and openlayers

--- htdocs/snippets.js
+++ htdocs/snippets.js
@@ -1,38 +1,81 @@
-var mapserv = '/cgi-bin/mapserv74';
-var mapfile = 'MAPSERVER_SNIPPETS';
+
+/** treats queryString search and hash equally */
+function urlParamsToJSON() {  // eslint-disable-line no-unused-vars
+    var
+      paramString = '',
+      pairs,
+      result = {};
+    if (window.location.search) {
+      paramString = decodeURIComponent(window.location.search).replace('?', '');
+    }
+  
+    if (window.location.hash) {
+      paramString += paramString ? '&' : '';
+      paramString += window.location.hash.replace('#', '');
+    }
+  
+    pairs = paramString.slice().split('&');
+    pairs.forEach(function(pair) {
+      pair = pair.split('=');
+      result[pair[0]] = decodeURIComponent(pair[1] || '');
+    });
+    return JSON.parse(JSON.stringify(result));
+  }
+
+var urlParams = urlParamsToJSON();
+var mapserv = urlParams.mapserv || '/cgi-bin/mapserv74';
+var mapfile = urlParams.mapfile || 'MAPSERVER_SNIPPETS';
+var baseurl = mapserv + '?MAP=' + mapfile;
+var center = urlParams.center ? JSON.parse(urlParams.center) : [30,40];
+var zoom = urlParams.zoom || 3;
+var wmslayers = urlParams.wmslayers;
+var tilelayers = urlParams.tilelayers;
+var wfslayer = urlParams.wfslayers;
+var wfsparams = '&SERVICE=WFS&VERSION=2.0.0&outputformat=geojson&REQUEST=getfeature&TYPENAME=';
+// see filter documentation https://mapserver.org/ogc/filter_encoding.html
+var wfsfilter = `&FILTER=<Filter><PropertyIsBetween><PropertyName>SCALERANK</PropertyName><LowerBoundary>0</LowerBoundary><UpperBoundary>1</UpperBoundary></PropertyIsBetween></Filter>`;
+
+// remove whitespace
+wfsfilter =  wfsfilter.replace(/\s/g, '');
 
 var snippets = [
     {
-      'id': 'color01',
-      'last_updated': '2019-05-21',
-      'author': '@sdlime',
-      'type': 'image',
-      'title': 'Specifiying Colors',
-      'description': 'MapServer supports a couple of ways of specifiying colors. Colors can be defined using either 8-bit (0-255) RGB tuples separated by spaces or a standard web hexidecimal string. In addition, hexidecimal colors can define an optional alpha value.',
-      'image_url': mapserv + '?map=' + mapfile + '&layer=color01&mode=map',
-      'mapfile_url': 'snippets/color01.snippet',
-      'tags': [ 'style', 'color' ]
-    },{
-	'id': 'regex01',
-	'last_updated': '2019-05-17',
-	'author': '@sdlime',
-	'type': 'image',
-	'title': 'Regex Class Expression',
-	'description': 'Simple filtering based on an attribute and a regular expression.',
-	'image_url': mapserv + '?map=' + mapfile + '&layer=regex01&mode=map',
-	'mapfile_url': 'snippets/regex01.snippet',
-	'tags': [ 'expression', 'regex' ]
-    },{
-	'id': 'regex02',
-	'last_updated': '2019-05-17',
-	'author': '@sdlime',
-	'type': 'image',
-	'title': 'Case-Insensitive Regex Class Expression',
-	'description': 'Simple filtering based on an attribute and a case-insensitive regular expression.',
-	'image_url': mapserv + '?map=' + mapfile + '&layer=regex02&mode=map',
-	'mapfile_url': 'snippets/regex02.snippet',
-	'tags': [ 'expression', 'regex' ]
-    },{
+        'id': 'color01',
+        'last_updated': '2019-05-21',
+        'author': '@sdlime',
+        'type': 'image',
+        'title': 'Specifiying Colors',
+        'description': 'MapServer supports a couple of ways of specifiying colors. Colors can be defined using either 8-bit (0-255) RGB tuples separated by spaces or a standard web hexidecimal string. In addition, hexidecimal colors can define an optional alpha value.',
+        'image_url': mapserv + '?map=' + mapfile + '&layer=color01&mode=map',
+        'mapfile_url': 'snippets/color01.snippet',
+        'leaflet_url':'leaflet.html?wmslayers=color01',
+        'openlayers_url': 'openlayers.html?wmslayers=color01',        
+        'tags': ['style', 'color']
+    }, {
+        'id': 'regex01',
+        'last_updated': '2019-05-17',
+        'author': '@sdlime',
+        'type': 'image',
+        'title': 'Regex Class Expression',
+        'description': 'Simple filtering based on an attribute and a regular expression.',
+        'image_url': mapserv + '?map=' + mapfile + '&layer=regex01&mode=map',
+        'mapfile_url': 'snippets/regex01.snippet',
+        'leaflet_url':'leaflet.html?wmslayers=regex01',
+        'openlayers_url': 'openlayers.html?wmslayers=regex01',        
+        'tags': ['expression', 'regex']
+    }, {
+        'id': 'regex02',
+        'last_updated': '2019-05-17',
+        'author': '@sdlime',
+        'type': 'image',
+        'title': 'Case-Insensitive Regex Class Expression',
+        'description': 'Simple filtering based on an attribute and a case-insensitive regular expression.',
+        'image_url': mapserv + '?map=' + mapfile + '&layer=regex02&mode=map',
+        'mapfile_url': 'snippets/regex02.snippet',
+        'leaflet_url':'leaflet.html?wmslayers=regex02',
+        'openlayers_url': 'openlayers.html?wmslayers=regex02',
+        'tags': ['expression', 'regex']
+    }, {
         'id': 'regex03',
         'last_updated': '2019-05-17',
         'author': '@sdlime',
@@ -41,8 +84,10 @@ var snippets = [
         'description': 'Simple filtering based on an attribute using a regular expression inside a logical expression.',
         'image_url': mapserv + '?map=' + mapfile + '&layer=regex03&mode=map',
         'mapfile_url': 'snippets/regex03.snippet',
-        'tags': [ 'expression', 'logical', 'regex' ]
-    },{
+        'leaflet_url':'leaflet.html?wmslayers=regex03',
+        'openlayers_url': 'openlayers.html?wmslayers=regex03',       
+        'tags': ['expression', 'logical', 'regex']
+    }, {
         'id': 'regex04',
         'last_updated': '2019-05-21',
         'author': '@sdlime',
@@ -51,6 +96,32 @@ var snippets = [
         'description': 'Simple filtering based on an attribute using a case-insensitive regular expression inside a logical expression.',
         'image_url': mapserv + '?map=' + mapfile + '&layer=regex04&mode=map',
         'mapfile_url': 'snippets/regex04.snippet',
-        'tags': [ 'expression', 'logical', 'regex' ]
+        'leaflet_url':'leaflet.html?wmslayers=regex04',
+        'openlayers_url': 'openlayers.html?wmslayers=regex04',        
+        'tags': ['expression', 'logical', 'regex']
+    }, {
+        'id': 'raster',
+        'last_updated': '2019-07-05',
+        'author': '@greenwoodmap',
+        'type': 'image',
+        'title': 'Raster Layer',
+        'description': 'Raster example',
+        'image_url': mapserv + '?map=' + mapfile + '&layer=hypsometric&mode=map',
+        'mapfile_url': 'snippets/hypsometric.snippet',
+        'leaflet_url':'leaflet.html?wmslayers=hypsometric',
+        'openlayers_url': 'openlayers.html?wmslayers=hypsometric',
+        'tags': ['raster', 'map file']
+    }, {
+        'id': 'popplace',
+        'last_updated': '2019-05-21',
+        'author': '@greenwood',
+        'type': 'image',
+        'title': 'point layer',
+        'description': 'Labeling at different scales',
+        'image_url': mapserv + '?map=' + mapfile + '&layer=populated-places&mode=map',
+        'mapfile_url': 'snippets/populated-places.snippet',
+        'leaflet_url':'leaflet.html?wmslayers=populated-places',
+        'openlayers_url': 'openlayers.html?wmslayers=populated-places',
+        'tags': ['point','label']
     }
 ];
